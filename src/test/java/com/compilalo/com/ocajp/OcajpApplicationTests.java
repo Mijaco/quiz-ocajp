75381250c92e76af5e37e7997939a743a226df72
Luis Enrique Neyra Majo <lneyra@profuturo.afp>
2019-07-22 12:28:45 -0500

    spring boot version app

---
+++ src/test/java/com/compilalo/com/ocajp/OcajpApplicationTests.java
@@ -0,0 +1,16 @@
+package com.compilalo.com.ocajp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OcajpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
